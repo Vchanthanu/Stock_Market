13812d8cfcb7c5007d09d3f1f48d4cbe536c8e97
Vchanthanu <v.chanthanu@gmail.com>
2022-05-08 15:21:13 +0530

    entity

---
+++ BackEnd/company/src/main/java/com/stockmarket/company/entity/Company.java
@@ -0,0 +1,11 @@
+package com.stockmarket.company.entity;
+
+
+public class Company {
+
+	public String co_code;
+	public String co_name;
+	public String co_ceo;
+	public int co_turnover;
+	public String co_website;
+}
