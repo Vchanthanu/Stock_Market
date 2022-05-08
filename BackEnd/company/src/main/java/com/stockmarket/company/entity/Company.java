d3ee8a08352f281b26c70bc68083143e83285ff0
Vchanthanu <61137425+Vchanthanu@users.noreply.github.com>
2022-05-08 15:40:58 +0530

    Company entity

--- BackEnd/company/src/main/java/com/stockmarket/company/entity/Company.java
+++ BackEnd/company/src/main/java/com/stockmarket/company/entity/Company.java
@@ -1,11 +1,56 @@
 package com.stockmarket.company.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name ="company")
 public class Company {
 
-	public String co_code;
-	public String co_name;
-	public String co_ceo;
-	public int co_turnover;
-	public String co_website;
+	@Id
+	@Column(name = "co_code")
+	public String code;
+	@Column(name = "co_name")
+	public String name;
+	@Column(name = "co_ceo")
+	public String ceo;
+	@Column(name = "co_turnover")
+	public int turnover;
+	@Column(name = "co_website")
+	public String website;
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCeo() {
+		return ceo;
+	}
+	public void setCeo(String ceo) {
+		this.ceo = ceo;
+	}
+	public int getTurnover() {
+		return turnover;
+	}
+	public void setTurnover(int turnover) {
+		this.turnover = turnover;
+	}
+	public String getWebsite() {
+		return website;
+	}
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+	
+	
+	
 }
