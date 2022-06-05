a530406fba8ca19bfff99f4d9f75f39adfbbe29f
Chanthanu V <61137425+Vchanthanu@users.noreply.github.com>
2022-06-05 22:31:51 +0530

    eureka,zuul(not),UIsearchcompany

--- BackEnd/company/src/main/java/com/stockmarket/company/CompanyApplication.java
+++ BackEnd/company/src/main/java/com/stockmarket/company/CompanyApplication.java
@@ -2,6 +2,7 @@ package com.stockmarket.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 @OpenAPIDefinition(info = @Info(title = "Company-API", version = "2.0", description = "Company Details"))
 @EnableMongoRepositories
 @SpringBootApplication
+@EnableEurekaClient
 public class CompanyApplication {
 
 	public static void main(String[] args) {
--- FrontEnd/stock-market/src/app/company/search-company/search-company.component.ts
+++ FrontEnd/stock-market/src/app/company/search-company/search-company.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'app-search-company',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-company.component.css']
 })
 export class SearchCompanyComponent implements OnInit {
-
+  public columnDefs: any;
+  public defaultColDef: any;
+  public rowData:[] = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.columnDefs = [
+      { name:'Company Code' ,field: 'companyCode' },
+      { name:'Company Name' ,field: 'companyName ' },
+      { name:'Stock Exchange' ,field: 'stockExchange ' },
+      { name:'Last Trade Price' ,field: 'price' }
+    ];
+    this.defaultColDef = {
+      sortable: true,
+      // filter: true,
+    };
   }
+  onGridReady(event:any){
 
+  }
 }
